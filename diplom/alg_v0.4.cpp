14e6482bbf805ff07cea9886e8d1160d35b30383
21zaber <zaber.eng@gmail.com>
2018-02-12 12:06:41 +0300

    changes in alg v0.4 for working with 32 or greater dims

--- diplom/alg_v0.4.cpp
+++ diplom/alg_v0.4.cpp
@@ -11,9 +11,71 @@
 using namespace std;
 
 #define VECTOR long long
-#define MAX_LEN 24
-#define HALF_LEN (MAX_LEN/2)
-#define MAX_VECTOR_COUNT (1 << MAX_LEN)
+#define VECTOR_COUNTER long long
+#define LENGTH_COUNTER int
+
+LENGTH_COUNTER MAX_LEN;
+LENGTH_COUNTER HALF_LEN;
+VECTOR_COUNTER MAX_VECTOR_COUNT;
+
+const VECTOR powers[56] = {
+    1,
+    2,
+    4,
+    8,
+    16,
+    32,
+    64,
+    128,
+    256,
+    512,
+    1024,
+    2048,
+    4096,
+    8192,
+    16384,
+    32768,
+    65536,
+    131072,
+    262144,
+    524288,
+    1048576,
+    2097152,
+    4194304,
+    8388608,
+    16777216,
+    33554432,
+    67108864,
+    134217728,
+    268435456,
+    536870912,
+    1073741824,
+    2147483648,
+    4294967296,
+    8589934592,
+    17179869184,
+    34359738368,
+    68719476736,
+    137438953472,
+    274877906944,
+    549755813888,
+    1099511627776,
+    2199023255552,
+    4398046511104,
+    8796093022208,
+    17592186044416,
+    35184372088832,
+    70368744177664,
+    140737488355328,
+    281474976710656,
+    562949953421312,
+    1125899906842624,
+    2251799813685248,
+    4503599627370496,
+    9007199254740992,
+    18014398509481984,
+    36028797018963968
+};
 
 
 // Random engine
@@ -27,7 +89,7 @@ VECTOR llrand() {
 } 
 
 
-VECTOR matrix[MAX_LEN];             
+VECTOR *matrix;             
 
 void print_vector(VECTOR v) {
     VECTOR t = v;
@@ -57,37 +119,56 @@ bool check(VECTOR a, VECTOR b) {
 
 
 int main() {
+    cin >> MAX_LEN;
+    HALF_LEN =  (MAX_LEN/2);
+    MAX_VECTOR_COUNT = powers[MAX_LEN];
+
     srand(time(NULL));
     clock_t tStart = clock();
 
-    memset(matrix, 0, sizeof(VECTOR) * MAX_LEN);
+    cout << "Memory allocation: ";
+
     VECTOR *cur_v, *new_v;
-    cur_v = (VECTOR *) malloc(sizeof(VECTOR) * MAX_VECTOR_COUNT / 2);
-    new_v = (VECTOR *) malloc(sizeof(VECTOR) * MAX_VECTOR_COUNT / 4);
+    matrix = (VECTOR *) malloc(sizeof(VECTOR) * MAX_LEN);
+    cur_v = (VECTOR *) malloc(sizeof(VECTOR) * MAX_VECTOR_COUNT / 4);
+    new_v = (VECTOR *) malloc(sizeof(VECTOR) * MAX_VECTOR_COUNT / 8);
 
-    int p = 0;
+    if (!cur_v || !new_v || !matrix) {
+        cout << "Failed.\n";
+        return 0;
+    }
 
-    for (int i = 0; i < MAX_VECTOR_COUNT; ++i) 
-        if (check(matrix[0], i))// && check(matrix[1], i))
+    cout << "Done.\n";
+
+    VECTOR_COUNTER p = 0;
+
+    for (VECTOR_COUNTER i = 0; i < MAX_VECTOR_COUNT; ++i) 
+        if (check(matrix[0], i))
             cur_v[p++] = i;
 
     cout << "Step 0. Current Current size of vector's list - " << p << endl;
 
-    for (int step = 1; step < MAX_LEN; ++step) {
+    for (LENGTH_COUNTER step = 1; step < MAX_LEN; ++step) {
         // choise random vector
         VECTOR v = cur_v[llrand() % p];
         matrix[step] = v;
-        int new_p = 0;
-        for (int i = 0; i < p; ++i){
+        VECTOR_COUNTER new_p = 0;
+
+        // calc new vectors
+        for (VECTOR_COUNTER i = 0; i < p; ++i){
             if (check(v, cur_v[i])) {
                 new_v[new_p++] = cur_v[i];    
             }
         }
+
+        // fail check
         if (step != MAX_LEN-1 && new_p < 1) {
             cout << "Fail on step " << step << endl;
             printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
             return 0;
         }
+
+        // iteration finish
         VECTOR *t = cur_v;
         cur_v = new_v;
         new_v = t;
